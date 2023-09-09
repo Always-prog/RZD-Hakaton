50be912aa25a94e244e4cbd748b01ca2665f5f37
Stepan <always.prog@gmail.com>
2023-09-09 13:14:51 +0300

    Added generating PDF UKZ

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 from flask import jsonify
+
+from report_generator import get_rpz_report
+
 app = Flask(__name__)
 
 
@@ -35,10 +38,10 @@ def download_file1():
 def download_file2():
     input_data = request.json
     print(input_data)
-    data = None
-    with open('./files/503р (1).pdf', 'rb') as f:
-        data = f.read()
-    return data
+
+    rpz_report = get_rpz_report(input_data)
+
+    return rpz_report
 
 
 if __name__ == '__main__':
