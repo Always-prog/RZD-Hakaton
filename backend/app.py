f2d68b35b9a5a64cff867e3ccabacf281dba1a6e
Evstifeev Mikhail <mievstifieiev1999@gmail.com>
2023-09-09 10:20:49 +0300

    Добавлены заглушки на скачивание файлов

--- backend/app.py
+++ backend/app.py
@@ -5,17 +5,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', show_files=False)
 
 
 @app.route('/result', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
-        data = request.json
-        print(data)
-        return jsonify({'message': "OK"})
+        try:
+            data = request.json
+            print(data)
+            return jsonify({'message': "OK"}), 200
+        except:
+            raise ValueError()
     else:
         return render_template("result.html")
+
+
+@app.route('/download/file1', methods=['POST'])
+def download_file1():
+    input_data = request.json
+    print(input_data)
+    data = None
+    with open('./files/file_10074349_signed.pdf', 'rb') as f:
+        data = f.read()
+    return data
+
+
+@app.route('/download/file2', methods=['POST'])
+def download_file2():
+    input_data = request.json
+    print(input_data)
+    data = None
+    with open('./files/503р (1).pdf', 'rb') as f:
+        data = f.read()
+    return data
 
 
 if __name__ == '__main__':
